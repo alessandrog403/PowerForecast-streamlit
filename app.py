4a4a2c70e5bf7d8f2bd683df4b98a73139801aa3
gael roy <gael.roy@hotmail.fr>
2026-03-17 17:10:32 +0100

    test pour vérifier que l'API fonctionne et est

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import datetime
 
 with st.form(key='params_for_api'):
     date_to_predict_price = st.date_input('date a laquelle vous souhaitez prédire un prix', value=datetime.datetime(2024,1,2))
+    lenght_of_prediction = st.number_input('nbre de jours pour lequelles vous voulez des prévisions horaires (0>nbre j > 2)', value=1)
 
     st.form_submit_button('Make prediction')
 
 params = dict(
-    date_to_predict_price=date_to_predict_price)
+    date=date_to_predict_price,
+    days=lenght_of_prediction)
 
-url = 'https://powerforecast-344702926535.europe-west1.run.app/predict/from_cache'
+url = 'https://power-forecast-344702926535.europe-west1.run.app/predict/from_cache'
 response = requests.get(url, params=params)

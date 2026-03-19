ed53fbf8d53dc5a659ce239fb2057717c93014c9
gael roy <gael.roy@hotmail.fr>
2026-03-19 12:48:14 +0100

    app corrigée

--- app.py
+++ app.py
@@ -27,15 +27,15 @@ st.caption("Sélectionnez une date de départ et le nombre de jours à prédire.
 
 
 with st.form(key='params_for_api'):
-    date_to_predict_price = st.date_input('date a laquelle vous souhaitez prédire un prix', value=datetime.datetime(2024,3,20))
-    lenght_of_prediction = st.number_input('nombre de jours pour lesquelles vous voulez des prévisions horaires', value=2)
+    date_to_predict_price = st.date_input('Date a laquelle vous souhaitez prédire un prix', value=datetime.datetime(2024,3,1))
+    lenght_of_prediction = st.number_input('Nombre de jours pour lesquelles vous voulez des prévisions horaires', value=2)
 
     if st.form_submit_button('Make prediction'):
 
         params = dict(
             date=date_to_predict_price,
             days=lenght_of_prediction)
-        
+
 
         url = 'https://power-forecast-344702926535.europe-west1.run.app/predict/combined'
         response = requests.get(url, params=params)
@@ -55,6 +55,6 @@ with st.form(key='params_for_api'):
         fig.add_trace(go.Scatter(x=dates, y=rnn, mode='lines', name='LSTM', line=dict(color='blue', width=3, dash='dash')))
         fig.add_trace(go.Scatter(x=dates, y=xgb, mode='lines', name='XGBoost', line=dict(color='orange', width=3, dash='dash')))
 
-        fig.update_layout(title='Prédictions de prix vs Prix réel', yaxis_title='Prix (EUR/MWh)', height=500)
+        fig.update_layout(title='Prix prédits vs Prix réels', yaxis_title='Prix (EUR/MWh)', height=500)
 
         st.plotly_chart(fig, use_container_width=True)
